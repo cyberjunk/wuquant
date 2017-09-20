308e60d6a3f89d8058aed934fadbb2b581a37ffc
Clint Banzhaf <clint.banzhaf@gmx.net>
2017-09-21 00:06:34 +0200

    WuQuantWrapTest

--- WuQuantWrapTest/Program.cs
+++ WuQuantWrapTest/Program.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace WuQuantWrapTest
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            IntPtr inst = WuQuantWrap.WuQuant.Create();
+
+            while(true) { }
         }
     }
 }
